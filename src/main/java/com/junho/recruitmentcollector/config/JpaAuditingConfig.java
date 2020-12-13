48d5d47abc5c99075d13b1277e7dcf3b2a6edb0c
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-12-13 22:30:20 +0900

    feat: 회사, 모집공고 엔티티 생성 (#2)

---
+++ src/main/java/com/junho/recruitmentcollector/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.junho.recruitmentcollector.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
