be12482c6179eb9244ab26e85f250fc5a2d6aa14
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-12-13 21:22:38 +0900

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# recruitment-collector
+내가 쓰려고 만드는 개발자 모집 공고 수집 서비스
---
+++ src/main/java/com/junho/recruitmentcollector/RecruitmentCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.junho.recruitmentcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitmentCollectorApplication.class, args);
+    }
+
+}
