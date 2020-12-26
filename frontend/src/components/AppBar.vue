76b926c1d97cefb3c7c3b0dba23beb488791e546
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-12-27 00:47:21 +0900

    채용공고 조회 화면 구성 (#10)
    * chore: eslint + prettier 설정
    
    * refactor: prettier에 따른 reformatting
    
    * feat: 화면 구성
    
    - 회사 선택, 채용공고 클릭시 이동 구현
    - 반응형 레이아웃
    - 상단바를 AppBar.vue, 내용을 Recruitments.vue으로 분리

---
+++ frontend/src/components/AppBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app-bar app color="white" flat>
+    <v-container class="py-0 fill-height">
+      <v-spacer></v-spacer>
+      <v-responsive max-width="260">
+        <v-text-field
+          dense
+          flat
+          hide-details
+          rounded
+          solo-inverted
+        ></v-text-field>
+      </v-responsive>
+    </v-container>
+    <v-avatar
+      class="mr-10 d-none d-sm-block"
+      color="grey darken-1"
+      size="32"
+    ></v-avatar>
+  </v-app-bar>
+</template>
+<script>
+export default {
+  name: "AppBar",
+
+  components: {},
+
+  data: () => ({}),
+  methods: {},
+};
+</script>
---
+++ frontend/src/components/Recruitments.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col sm="2">
+        <v-sheet rounded="lg">
+          <v-list color="transparent">
+            <v-list-item-group v-model="selectedCompany" color="dark">
+              <v-list-item link>
+                <v-list-item-content>
+                  <v-list-item-title> 전체보기 </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-divider class="my-2"></v-divider>
+              <v-list-item v-for="(company, idx) in companies" :key="idx" link>
+                <v-list-item-content>
+                  <v-list-item-title> {{ company }} </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-sheet>
+      </v-col>
+      <v-col sm="10">
+        <v-sheet min-height="70vh" rounded="lg">
+          <v-list-item
+            v-for="(recruitment, idx) in recruitments"
+            :key="idx"
+            link
+            @click="goTo(recruitment.url)"
+          >
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ recruitment.title }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  name: "Recruitments",
+
+  components: {},
+
+  data: () => ({
+    selectedCompany: 1,
+    companies: ["네이버", "NHN"],
+    recruitments: [
+      {
+        title:
+          "네이버 예약 플랫폼 프론트엔드/백엔드 개발(채용연계형 인턴십 2020.12)",
+        url:
+          "https://recruit.navercorp.com/naver/job/detail/developer?annoId=20004943&classId=&jobId=&entTypeCd=&searchTxt=&searchSysComCd=",
+      },
+    ],
+  }),
+  methods: {
+    goTo(url) {
+      window.open(url, "_blank");
+    },
+  },
+};
+</script>
